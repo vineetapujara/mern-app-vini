1eb123cc92aec0a3195a45350971f34bae6ea7d3
vineetapujara <61109317+vineetapujara@users.noreply.github.com>
2021-06-04 13:42:14 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+const App = () => {
+  return (
+    <Router>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+          <Route />
+        </Switch>
+      </Container>
+    </Router>
+  );
+};
+
+export default App;
